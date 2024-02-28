61044a6015de7cef72a36da7ccedacb7b37343d7
TravisBoyd884 <phtboyd884@gmail.com>
2024-02-28 14:06:56 -0500

    Add asm support

---
+++ lua/travis/plugins/tree-sitter-asm.lua
@@ -0,0 +1,1 @@
+return { "rush-rs/tree-sitter-asm" }
--- lua/travis/plugins/treesitter.lua
+++ lua/travis/plugins/treesitter.lua
@@ -1,15 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  }
+		configs.setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
 
+		require("nvim-treesitter.parsers").get_parser_configs().asm = {
+			install_info = {
+				url = "https://github.com/rush-rs/tree-sitter-asm.git",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+		}
+	end,
+}
